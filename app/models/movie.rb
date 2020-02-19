5c47e71b0353551cde1e60a04c5d8ee7a029c0f9
AdityaVijayvergia <vijayvergia.aditya15@gmail.com>
2020-02-19 04:16:14 +0000

    movie.rb changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+    def self.get_rating_list
+      
+      sol = {}
+  	  self.select(:rating).uniq.each do |movie|
+  		result[movie.rating]=1
+	  end
+  	  sol
+    end
 end
